935479448cf21b17545752a5ec6bed46b75aa4d3
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-03-17 20:27:57 -0400

    update story section

--- components/Story.js
+++ components/Story.js
@@ -1,8 +1,12 @@
 function Story({ img, username }) {
   return (
     <div>
-      <img src={img} alt="user picture" />
-      <p>{username}</p>
+      <img
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"
+        src={img}
+        alt="user picture"
+      />
+      <p className="text-xs w-14 truncate">{username}</p>
     </div>
   );
 }
