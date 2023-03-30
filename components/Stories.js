2bd0940a6a5f3485817a5950ebe957b24dc25061
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-03-30 16:01:59 -0400

    modify story components

--- components/Stories.js
+++ components/Stories.js
@@ -2,9 +2,11 @@ import minifaker from "minifaker";
 import "minifaker/locales/en";
 import { useEffect, useState } from "react";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 
 function Stories() {
   const [story, setStory] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const storyUsers = minifaker.array(20, (i) => ({
@@ -17,6 +19,13 @@ function Stories() {
 
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
+      {session && (
+        <Story
+          img={session.user.image}
+          username={session.user.username}
+          isUser="true"
+        />
+      )}
       {story.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
